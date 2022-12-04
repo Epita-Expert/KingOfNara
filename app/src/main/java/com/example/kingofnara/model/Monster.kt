ce5f9cd44f3545e66f6d4458c91c1aa9b982e000
taugat <auguste.hatton@gmail.com>
2022-12-04 05:13:27 -0800

    rename monsters

--- app/src/main/java/com/example/kingofnara/model/Monster.kt
+++ app/src/main/java/com/example/kingofnara/model/Monster.kt
@@ -4,12 +4,12 @@ import com.example.kingofnara.R
 
 enum class Monster(val resourceDraw: Int)
 {
-    MONSTER_1(R.drawable.monster1),
-    MONSTER_2(R.drawable.monster2),
-    MONSTER_3(R.drawable.monster3),
+    BAVE(R.drawable.monster1),
+    BLUP(R.drawable.monster2),
+    ROUGE(R.drawable.monster3),
     MONSTER_4(R.drawable.monster4),
-    MONSTER_5(R.drawable.monster5),
-    MONSTER_6(R.drawable.monster6);
+    DRAGON(R.drawable.monster5),
+    LUPIOTE(R.drawable.monster6);
 
 }
 
